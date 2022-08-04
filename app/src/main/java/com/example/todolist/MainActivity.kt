552a3b8e734ddd9bc93135cea59360bf7058995e
Roxana Tot <roxana.tot@gmail.com>
2022-08-04 23:11:20 +0100

    Scroll to first unchecked item

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -11,17 +11,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var todoList = mutableListOf(
-            Todo("Read a chapter", false),
+            Todo("Read a chapter", true),
             Todo("Do a lesson on Duolingo", true),
             Todo("Do a commit", true),
             Todo("Practice on ukulele", false),
             Todo("Go for a walk", true),
             Todo("Cook dinner", true),
+            Todo("Exercise", true),
+            Todo("Play Overcooked", false),
+            Todo("Edit a video", false),
+            Todo("Learn something new", true),
+            Todo("Write in the journal", false),
         )
 
         val adapter = TodoAdapter(todoList)
         rvTodos.adapter = adapter
         rvTodos.layoutManager = LinearLayoutManager(this)
+        rvTodos.scrollToPosition(findFirstUncheckedItem(todoList))
 
         btnAddTodo.setOnClickListener {
             val title = etTodo.text.toString()
@@ -29,5 +35,15 @@ class MainActivity : AppCompatActivity() {
             todoList.add(todo)
             adapter.notifyItemChanged(todoList.size - 1)
         }
+
+    }
+
+    fun findFirstUncheckedItem(todoList: List<Todo>): Int{
+        for ((pos, todo) in todoList.withIndex()) {
+            if (!todo.isChecked) {
+                return pos
+            }
+        }
+        return 0
     }
 }
