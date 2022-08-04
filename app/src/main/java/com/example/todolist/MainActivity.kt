901232bcff83d1ed8521a0533765d104af930828
Roxana Tot <roxana.tot@gmail.com>
2022-08-04 22:41:31 +0100

    Make todo list

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var todoList = mutableListOf(
+            Todo("Read a chapter", false),
+            Todo("Do a lesson on Duolingo", true),
+            Todo("Do a commit", true),
+            Todo("Practice on ukulele", false),
+            Todo("Go for a walk", true),
+            Todo("Cook dinner", true),
+        )
+
+        val adapter = TodoAdapter(todoList)
+        rvTodos.adapter = adapter
+        rvTodos.layoutManager = LinearLayoutManager(this)
+
+        btnAddTodo.setOnClickListener {
+            val title = etTodo.text.toString()
+            val todo = Todo(title, false)
+            todoList.add(todo)
+            adapter.notifyItemChanged(todoList.size - 1)
+        }
     }
 }
